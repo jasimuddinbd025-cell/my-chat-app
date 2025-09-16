24f790692efa2aa5145a29941e7d69e6ef900c34
Jasim Uddin <jasimuddinbd025@gmail.com>
2025-09-17 00:21:10 +0600

    Fix Vite SPA for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-chat-app/' // <-- আপনার repo নাম
+  base: './'  // relative paths for production
 })
