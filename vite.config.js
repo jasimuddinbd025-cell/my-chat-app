074ba8d978bac3468c1f9d36aac17704f932c203
Jasim Uddin <jasimuddinbd025@gmail.com>
2025-09-17 02:41:59 +0600

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// Externalize supabase to fix build
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      external: ["@supabase/supabase-js"]
-    }
-  }
+  base: './'
 });
