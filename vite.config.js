2f140609cc94764aa26574f8d47e88ec2cc01707
Jasim Uddin <jasimuddinbd025@gmail.com>
2025-09-17 01:51:28 +0600

    Deploy-ready config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // important for SPA to work on Vercel
+  base: "./", // Important for SPA assets in Vercel
+  build: {
+    outDir: "dist"
+  }
 });
